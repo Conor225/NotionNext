e1eb2c6e508b2c08b9c9fddad5c6d3ffd905e1cc
Conor225 <1240753014@qq.com>
2024-11-12 12:56:13 +0800

    Update SideBar.js

--- themes/example/components/SideBar.js
+++ themes/example/components/SideBar.js
@@ -9,7 +9,57 @@ import Catalog from './Catalog'
 const ExampleRecentComments = dynamic(
   () => import('./RecentCommentListForExample')
 )
+/**
+ * 侧边栏
+ */
+export const SideBar = props => {
+  const { locale } = useGlobal()
+  const { latestPosts, categoryOptions, notice, post } = props
+  // 评论相关
+  const COMMENT_WALINE_SERVER_URL = siteConfig(
+    'COMMENT_WALINE_SERVER_URL',
+    false
+  )
+  const COMMENT_WALINE_RECENT = siteConfig('COMMENT_WALINE_RECENT', false)
 
+  // 文章详情页特殊布局
+  const HIDDEN_NOTIFICATION =
+    post && siteConfig('EXAMPLE_ARTICLE_HIDDEN_NOTIFICATION', false, CONFIG)
+
+  // 文章详情页左右布局改为上下布局
+  const LAYOUT_VERTICAL =
+    post && siteConfig('EXAMPLE_ARTICLE_LAYOUT_VERTICAL', false, CONFIG)
+
+  return (
+    <>
+      
+
+     
+
+      {/* 公告 */}
+      {/* 公告栏 */}
+      {!HIDDEN_NOTIFICATION && <Announcement post={notice} />}
+
+      {/* 最近评论 */}
+      {COMMENT_WALINE_SERVER_URL && COMMENT_WALINE_RECENT && (
+        <aside className='w-full rounded shadow overflow-hidden mb-6'>
+          <h3 className='text-sm bg-gray-100 text-gray-700 dark:bg-hexo-black-gray dark:text-gray-200 py-3 px-4 dark:border-hexo-black-gray border-b'>
+            {locale.COMMON.RECENT_COMMENTS}
+          </h3>
+
+          <div className='p-4'>
+            <ExampleRecentComments />
+          </div>
+        </aside>
+      )}
+
+      {/* 宠物挂件 */}
+      <aside
+        className={`rounded overflow-hidden mb-6 ${LAYOUT_VERTICAL ? 'hidden md:fixed right-4 bottom-20' : ''}`}>
+        <Live2D />
+      </aside>
+    </>
+  )
 
 }
 
