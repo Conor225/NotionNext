b3ff5e2d63023f793b50403175f08ca646a74f2c
Conor225 <1240753014@qq.com>
2024-11-12 14:12:35 +0800

    Update SideBar.js

--- themes/example/components/SideBar.js
+++ themes/example/components/SideBar.js
@@ -9,7 +9,6 @@ import Catalog from './Catalog'
 const ExampleRecentComments = dynamic(
   () => import('./RecentCommentListForExample')
 )
-
 /**
  * 侧边栏
  */
@@ -33,94 +32,3 @@ export const SideBar = props => {
 
   return (
     <>
-      {/* 目录 */}
-      {post?.toc && post?.toc.length > 2 && (
-        <aside className='w-full rounded shadow overflow-hidden mb-6 pb-4'>
-          <h3 className='text-sm bg-gray-100 text-gray-700 dark:bg-hexo-black-gray dark:text-gray-200 py-3 px-4 dark:border-hexo-black-gray border-b'>
-            {locale.COMMON.TABLE_OF_CONTENTS}
-          </h3>
-          <Catalog toc={post?.toc} />
-        </aside>
-      )}
-
-      {/* 分类 */}
-      <aside className='w-full rounded shadow overflow-hidden mb-6'>
-        <h3 className='text-sm bg-gray-100 text-gray-700 dark:bg-hexo-black-gray dark:text-gray-200 py-3 px-4 dark:border-hexo-black-gray border-b'>
-          {locale.COMMON.CATEGORY}
-        </h3>
-
-        <div className='p-4'>
-          <ul className='list-reset leading-normal'>
-            {categoryOptions?.map(category => {
-              return (
-                <Link
-                  key={category.name}
-                  href={`/category/${category.name}`}
-                  passHref
-                  legacyBehavior>
-                  <li>
-                    {' '}
-                    <a
-                      href={`/category/${category.name}`}
-                      className='text-gray-darkest text-sm hover:underline'>
-                      {category.name}({category.count})
-                    </a>
-                  </li>
-                </Link>
-              )
-            })}
-          </ul>
-        </div>
-      </aside>
-
-      {/* 最新文章 */}
-      <aside className='w-full rounded shadow overflow-hidden mb-6'>
-        <h3 className='text-sm bg-gray-100 text-gray-700 dark:bg-hexo-black-gray dark:text-gray-200 py-3 px-4 dark:border-hexo-black-gray border-b'>
-          {locale.COMMON.LATEST_POSTS}
-        </h3>
-
-        <div className='p-4'>
-          <ul className='list-reset leading-normal'>
-            {latestPosts?.map(p => {
-              return (
-                <Link key={p.id} href={`/${p.slug}`} passHref legacyBehavior>
-                  <li>
-                    {' '}
-                    <a
-                      href={`/${p.slug}`}
-                      className='text-gray-darkest text-sm hover:underline'>
-                      {p.title}
-                    </a>
-                  </li>
-                </Link>
-              )
-            })}
-          </ul>
-        </div>
-      </aside>
-
-      {/* 公告 */}
-      {/* 公告栏 */}
-      {!HIDDEN_NOTIFICATION && <Announcement post={notice} />}
-
-      {/* 最近评论 */}
-      {COMMENT_WALINE_SERVER_URL && COMMENT_WALINE_RECENT && (
-        <aside className='w-full rounded shadow overflow-hidden mb-6'>
-          <h3 className='text-sm bg-gray-100 text-gray-700 dark:bg-hexo-black-gray dark:text-gray-200 py-3 px-4 dark:border-hexo-black-gray border-b'>
-            {locale.COMMON.RECENT_COMMENTS}
-          </h3>
-
-          <div className='p-4'>
-            <ExampleRecentComments />
-          </div>
-        </aside>
-      )}
-
-      {/* 宠物挂件 */}
-      <aside
-        className={`rounded overflow-hidden mb-6 ${LAYOUT_VERTICAL ? 'hidden md:fixed right-4 bottom-20' : ''}`}>
-        <Live2D />
-      </aside>
-    </>
-  )
-}
